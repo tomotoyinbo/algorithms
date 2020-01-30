c060a5cd93ebe6bec78f9905db90bcf7ad29b422
temitayoomotoyinbo <tomotoyinbo@gmail.com>
2020-01-30 07:42:23 -0800

    Add instance variable to class

--- src/main/java/datastructures/DynamicArray.java
+++ src/main/java/datastructures/DynamicArray.java
@@ -1,5 +1,10 @@
 package datastructures;
 
-public class DynamicArray {
+public class DynamicArray<String> {
 
+    private Object[] data;
+
+    private int size = 0;
+
+    private int initialCapacity = 0;
 }
