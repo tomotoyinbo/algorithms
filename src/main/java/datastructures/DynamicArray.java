f4f08a08fa603b2ce9d741277dfc3bef5e3e6da6
temitayoomotoyinbo <tomotoyinbo@gmail.com>
2020-01-30 07:52:04 -0800

    Add implementation & test skeleton

--- src/main/java/datastructures/DynamicArray.java
+++ src/main/java/datastructures/DynamicArray.java
@@ -7,4 +7,52 @@ public class DynamicArray<String> {
     private int size = 0;
 
     private int initialCapacity = 0;
+
+    public DynamicArray(int initialCapacity) {
+        this.initialCapacity = initialCapacity;
+        data = new Object[initialCapacity];
+    }
+
+    public String get(int index){
+
+    }
+
+    public void set(int index, String value){
+
+    }
+
+    public void insert(int index, String value){
+
+    }
+
+    public void delete(int index) {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public boolean Contains(String value) {
+    }
+
+    private void resize() {
+
+        Object[] newData = new Object[initialCapacity * 2];
+
+        for (int i = 0; i < initialCapacity; i++) {
+            newData[i] = data[i];
+        }
+
+        data = newData;
+        initialCapacity = initialCapacity * 2;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void print() {
+        for (int i=0;i< size;i++) {
+            System.out.println("data[i] = " + data[i]);
+        }
+    }
 }
--- src/test/java/datastructures/DynamicArrayTest.java
+++ src/test/java/datastructures/DynamicArrayTest.java
@@ -1,5 +1,39 @@
 package datastructures;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class DynamicArrayTest {
 
+    private DynamicArray array;
+
+    @Before
+    public void SetUp() {
+        array = new DynamicArray<String>(2);
+    }
+
+    @Test
+    public void GetAndSet() {
+
+    }
+
+    @Test
+    public void Insert() {
+
+    }
+
+    @Test
+    public void DeleteLast() {
+
+    }
+
+    @Test
+    public void isEmpty() {
+
+    }
+
+    @Test
+    public void Contains()  {
+
+    }
 }
